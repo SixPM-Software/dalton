1d2ab61b92c6eced5ce3931e14ab51c3886608e7
stuckatsixpm <80179622+stuckatsixpm@users.noreply.github.com>
2021-03-13 22:54:06 +1000

    ci: remove testpypi

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2021, stuckatsixpm"
 author = "stuckatsixpm"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1-alpha.2"
+release = "0.1.1-alpha.3"
 
 
 # -- General configuration ---------------------------------------------------
